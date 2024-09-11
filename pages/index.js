437b5d6b81818f7a08ef75e21c3b5141cf8d21d3
Hayden D’Souza <haydenfds@gmail.com>
2024-09-11 15:07:38 -0700

    Added routes + components for experience, skills, projects Fucked up the formatting of home page - need to fix this added extra color schemes to tailwind config

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,19 @@ export default function HomePage() {
             <p className='mt-4 text-xl'>I just graduated with a B.S. in Computer Science from UCLA.<br /> I&apos;m looking for entry level positions in technical roles.<br /> I am most familiar with JavaScript and Python. </p>
             <p className='mt-4 text-xl'>Some more useless information about me.</p>
             <p className='mt-4 text-xl'>I like to do X in my free time.</p>
+            <p className='mt-4 text-xl'>Here is my CV for your reference.</p>
+        </section>
+        <section className="mt-20">
+            <h1 className='section-title'>Projects</h1>
+            <div className='mx-auto p-4 bg-card-dark border-card-border border-2 rounded-md flex-1 w-1/2 max-w-[calc(50%-10px)]'>
+                <h1>Project 1</h1>
+            </div>
+            <div className='mx-auto p-4 bg-card-dark border-card-border border-2 rounded-md flex-1 w-1/2 max-w-[calc(50%-10px)]'>
+                <h1>Project 2</h1>
+            </div>                
+            <div className='mx-auto p-4 bg-card-dark border-card-border border-2 rounded-md flex-1 w-1/2 max-w-[calc(50%-10px)]'>
+                <h1>Project 3</h1>
+            </div>                                                
         </section>
     </Layout>
   );
