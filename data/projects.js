5dead4d53ed6f7d7361df4d42d24aa77ed9d4c7d
Hayden D’Souza <haydenfds@gmail.com>
2024-09-11 23:56:16 -0700

    Added prelim projects data w/ formatting on home page

---
+++ data/projects.js
@@ -0,0 +1,40 @@
+export const projects = [
+    { 
+        id: 1, 
+        title: 'Duplify', 
+        description: 'duplify website',
+        github: 'https://github.com/haydenfd/duplify-app',
+        url: 'http://duplify.s3-website-us-west-1.amazonaws.com/',
+    },
+    { 
+        id: 2, 
+        title: 'Seatcheck', 
+        description: 'seatcheck website',
+        github: 'https://github.com/haydenfd/seatcheck',
+        url: 'http://seatcheck.s3-website-us-west-1.amazonaws.com/',
+    },
+    { 
+        id: 3, 
+        title: 'Duplify', 
+        description: '',
+        github: 'https://github.com/haydenfd/duplify-app',
+    },
+    { 
+        id: 4, 
+        title: 'Duplify', 
+        description: 'duplify website',
+        github: 'https://github.com/haydenfd/duplify-app',
+    },
+    { 
+        id: 5, 
+        title: 'Duplify', 
+        description: 'duplify website',
+        github: 'https://github.com/haydenfd/duplify-app',
+    },      
+    { 
+        id: 6, 
+        title: 'Duplify', 
+        description: 'duplify website',
+        github: 'https://github.com/haydenfd/duplify-app',
+    },            
+  ];
