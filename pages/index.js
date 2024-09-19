d162277ae9554b32a7dec4b4d0bb1aa8bb0466b4
Hayden D’Souza <haydenfds@gmail.com>
2024-09-19 07:15:59 -0700

    More landing page formats

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,14 @@ export default function HomePage() {
           height={400}
         /> */}
         <div className="flex-1">
-          <h2 className="text-3xl font-bold">Hey, I&apos;m Hayden</h2>
+          <h2 className="text-3xl font-bold">Hey, I&apos;m Hayden 👋</h2>
           <p className="mt-4 text-xl">
-            I just graduated with a B.S. in Computer Science from UCLA.<br />
-            I am most familiar with JavaScript (React, Node) and Python (FastAPI).<br /> 
-            I&apos;m looking for entry level positions in technical roles.<br /> 
-            My classes at UCLA exposed me to various areas of interest, including Deep Learning, Web Development, Databases, and 
-          </p>
+            I just graduated with a <p className="emphasized-text">B.S. in Computer Science</p> from <p className="emphasized-text">UCLA</p>.</p>
+            <p className="mt-4 text-xl">I am most familiar with JavaScript (React, Node) and Python (FastAPI).</p>
+            <p className="mt-4 text-xl">I&apos;m looking for entry level positions in technical roles.</p>        
           <p className="mt-4 text-xl">In my free time, I&apos;m discovering new EDM songs, complaining about the Lakers, working out, and trying to brew the perfect cup of coffee.</p>
           <p className="mt-4 text-xl">
-            You can find my CV <a className="inline underline text-primary-yellow-color hover:text-primary-yellow-color-hover" href="/resume.pdf" target="_blank" rel="noopener norefferer">here</a>.
+            You can find my CV <a className="inline underline text-primary-yellow-color hover:text-primary-yellow-color-hover font-medium" href="/resume.pdf" target="_blank" rel="noopener norefferer">here</a>.
           </p>
         </div>
       </section>
