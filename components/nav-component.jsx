782256e4f7cde701d5bbea3e80b81e1ea613986b
Hayden D’Souza <haydenfds@gmail.com>
2024-11-19 17:03:03 -0800

    Feedback changes, prelim

--- components/nav-component.jsx
+++ components/nav-component.jsx
@@ -1,9 +1,21 @@
 import Link from 'next/link';
-
-import { useRouter } from 'next/router';
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { IoDocumentTextSharp } from 'react-icons/io5';
 
 export const Navbar = () => {
-  const router = useRouter();
+
+  const copyText = (text) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        alert("Copied haydenfds@gmail.com to clipboard!");
+      },
+      (err) => {
+        console.error("Failed to copy text: ", err);
+      }
+    );
+  };
 
   return (
     <nav className="px-0 top-0 left-0 w-full z-[100] opacity-100 bg-bg-dark">
@@ -15,16 +27,24 @@ export const Navbar = () => {
             </h1>
           </Link>
         </div>
-        <div className="space-x-6 font-roboto-condensed">
-          <Link href="/experience" className={`navbar-section-btn ${router.pathname === '/experience' ? 'active' : ''}`}>
-            Experience
-          </Link>
-          <Link href="/projects" className={`navbar-section-btn ${router.pathname === '/projects' ? 'active' : ''}`}>
-            Projects
-          </Link>
-          <Link href="/resume.pdf" rel="noopener noreferrer" target="_blank" className={`navbar-section-btn ${router.pathname === '/resume' ? 'active' : ''}`}>
-            Resume
-          </Link>
+        <div className="gap-11 font-roboto-condensed mr-4 flex">
+        <a href="https://github.com/haydenfd" target="_blank" rel="noopener noreferrer" className="navbar-contact-btn">
+          <AiFillGithub className="scale-[225%]" />
+        </a>
+        <a href="https://www.linkedin.com/in/hayden-dsouza/" target="_blank" rel="noopener noreferrer" className="navbar-contact-btn">
+          <FaLinkedin className="scale-[200%]"/>
+        </a>                   
+        <a href="#" className="navbar-contact-btn"
+            onClick={(e) => {
+            e.preventDefault(); 
+            copyText("haydenfds@gmail.com");
+            }}
+            >
+                <IoMdMail className="scale-[250%]"/>
+            </a>           
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className='navbar-contact-btn'>
+                <IoDocumentTextSharp className="scale-[200%]"/>
+            </a>            
         </div>
       </div>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,12 @@
-import Link from "next/link";
 import { projects } from '../data/projects';
 import GenerateProjects from "utils/generateProjects";
-import { AiFillGithub } from "react-icons/ai";
-import { FaLinkedin } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
 
-export default function HomePage() {
-
-  const copyText = (text) => {
-    navigator.clipboard.writeText(text).then(
-      () => {
-        alert("Copied haydenfds@gmail.com to clipboard!");
-      },
-      (err) => {
-        console.error("Failed to copy text: ", err);
-      }
-    );
-  };
+export default function Landing() {
 
   return (
     <>
-      <section className="mt-20 flex flex-col lg:flex-row-reverse items-start gap-8 w-full leading-relaxed">
-        <div className="flex-1">
+      <section className="page-section">
+        <div>
           <h2 className="text-3xl font-bold">Hey, I&apos;m Hayden 👋</h2>
             <p className="mt-4 text-lg">I just graduated from UCLA with a Bachelor&apos;s in Computer Science.</p>
             <p className="mt-4 text-lg">Currently looking for new grad positions in Software Engineering, Data Science, and other similar technical roles.</p>
@@ -39,30 +24,9 @@ export default function HomePage() {
           </p> */}
         </div>
       </section>
-      <section className="w-1/2 flex flex-row flex-nowrap my-20 gap-8 mx-auto justify-center">
-        <a href="https://github.com/haydenfd" target="_blank" rel="noopener norefferer" className="flex flex-row justify-between gap-4 items-center p-3 rounded-lg text-base-button-color bg-card-dark border-2 border-card-border hover:border-primary-yellow-color hover:text-primary-yellow-color">
-          <AiFillGithub className="scale-[175%] flex-1" />
-          <p className="text-lg font-medium flex-1">GitHub</p>
-        </a>
-        <a href="https://www.linkedin.com/in/hayden-dsouza/" className="flex flex-row justify-between gap-4 items-center p-3 rounded-lg text-base-button-color bg-card-dark border-2 border-card-border hover:border-primary-yellow-color hover:text-primary-yellow-color" target="_blank" rel="noopener norefferer">
-          <FaLinkedin className="scale-[175%] flex-1 ml"/>
-          <p className="text-lg font-medium flex-1">LinkedIn</p>
-        </a>                
-        <a href="#" className="flex flex-row justify-between gap-4 items-center p-3 rounded-lg text-base-button-color bg-card-dark border-2 border-card-border hover:border-primary-yellow-color hover:text-primary-yellow-color"
-            onClick={(e) => {
-            e.preventDefault(); 
-            copyText("haydenfds@gmail.com");
-            }}
-            
-            >
-                <IoMdMail className="scale-[175%] flex-1"/>
-                <p className="text-lg font-medium flex-1">Email</p>
-            </a>                 
-      </section>
-      <section>
-        <div className="flex w-full justify-between items-center">
+      <section className='page-section'>
+        <div className="w-full">
           <h1 className="section-title">Experience</h1>
-          <Link href="/experience" className="section-button">All Experience</Link>
         </div>
         <div className="my-6">
             <div className="w-full py-4 px-3 border-2 text-white bg-card-dark border-card-border rounded-lg mb-4 flex justify-between">
@@ -83,10 +47,9 @@ export default function HomePage() {
             </div>
         </div>
       </section>      
-      <section className="">
-        <div className="flex w-full justify-between mt-20 items-center">
+      <section className="page-section">
+        <div className="w-full">
           <h1 className="section-title">Projects</h1>
-          <Link href="/projects" className="section-button">All Projects</Link>
         </div>
         <GenerateProjects limit={6} projects={projects} />
       </section>
